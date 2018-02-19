80fb1780eb737bc6cd5b68946f950d56992f19fc
Emmanuel Medina <ing.emmanuelmedina@gmail.com>
2018-02-18 22:58:57 -0600

    Modified the way that the bytes were displayed to properly show in TextBox controls

--- HashedPassPreview/HashedPassPreview/frmPBKDF2.cs
+++ HashedPassPreview/HashedPassPreview/frmPBKDF2.cs
@@ -23,7 +23,7 @@ namespace HashedPassPreview
             byte[] salt;
             new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
 
-            txtSaltBytes.Text = salt.ToString();
+            txtSaltBytes.Text = BytesToString(salt);
             txtSaltString.Text = Convert.ToBase64String(salt);
 
             string pwd = txtPwd.Text;
@@ -33,7 +33,7 @@ namespace HashedPassPreview
 
             byte[] hash = pbkdf2.GetBytes(20);
 
-            txtHashedPasswordBytes.Text = hash.ToString();
+            txtHashedPasswordBytes.Text = BytesToString(hash);
             txtHashedPasswordString.Text = Convert.ToBase64String(hash);
 
             byte[] hashBytes = new byte[36];
@@ -41,8 +41,20 @@ namespace HashedPassPreview
             Array.Copy(salt, 0, hashBytes, 0, 16);
             Array.Copy(hash, 0, hashBytes, 16, 20);
 
-            txtResultBytes.Text = hashBytes.ToString();
+            txtResultBytes.Text = BytesToString(hashBytes);
             txtResultString.Text = Convert.ToBase64String(hashBytes);
+        }
+
+        private string BytesToString(byte[] bytes)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                result += "[" + bytes[i].ToString() + "]";
+            }
+
+            return result;
         }
     }
 }
